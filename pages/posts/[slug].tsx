8248f0e7378cd7bcc648f99ac5b2608ecff531c2
hide <mono0110@gmail.com>
2024-11-07 17:34:33 +0900

    mod

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -47,7 +47,7 @@ const Post = ({ post }) => {
                         const match = /language-(\w+)/.exec(className || '')
                         return match ? (
                             <SyntaxHighlighter
-                                {...rest}
+
                                 PreTag="div"
                                 children={String(children).replace(/\n$/, '')}
                                 language={match[1]}
